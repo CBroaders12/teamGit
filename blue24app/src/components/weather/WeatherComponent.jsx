bdffc2810635561ad692d4ed9335635bed663b6a
CBroaders <67294112+CBroaders12@users.noreply.github.com>
2020-10-26 19:40:19 -0400

    fetch on page load

--- blue24app/src/components/weather/WeatherComponent.jsx
+++ blue24app/src/components/weather/WeatherComponent.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Card, CardBody, CardTitle, CardText, CardSubtitle, CardImg, CardHeader, Button } from 'reactstrap';
 
 const WeatherComponent = props => {
 
@@ -9,30 +10,47 @@ const WeatherComponent = props => {
 
   const [ latitude, setLatitude ] = useState(myLocation.lat);
   const [ longitude, setLongitude ] = useState(myLocation.long);
-  const [ currentWeather, setCurrentWeather ] = useState({});
+  const [ temp, setTemp ] = useState('');
+  const [ description, setDescription ] = useState('');
+  const [ isImperial, setIsImperial ] = useState(true);
+  const [ icon, setIcon ] = useState('');
+  const [ unitType, setUnitType ] = useState('imperial');
+
+  const toggleUnitType = () => unitType === "imperial" ? setUnitType("metric") : setUnitType("imperial");
+
+  const toggleTempScale = () => {
+    setIsImperial(!isImperial);
+    toggleUnitType();
+  }
 
   const apiKey = "a7688b921c49078b59d83b229dee0a22";
-  // const weatherURL = `api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
-  const weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&exclude=minutely,hourly,daily,alerts&units=imperial`
+  const weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&exclude=minutely,hourly,daily,alerts`
 
   const fetchWeather = () => {
-    fetch(weatherURL)
+    fetch(`${weatherURL}&units=${unitType}`)
       .then(response => response.json())
       .then(json => {
         console.log(json.current);
-        setCurrentWeather(json.current)
+        setTemp(Math.round(json.current.temp));
+        setDescription(json.current.weather[0].main);
+        setIcon(json.current.weather[0].icon);
       })
   };
 
-  // useEffect(fetchWeather, []);
+  useEffect(fetchWeather, []);
 
   return(
-    <>
-    <div>
-      <h1>Weather</h1>
-      <button onClick={fetchWeather}>Log weather</button>
-    </div>
-    </>
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-center font-weight-bold">{description}</CardTitle>
+      </CardHeader>
+      <CardImg src={`http://openweathermap.org/img/wn/${icon}@2x.png`} />
+      <CardBody className="text-center">
+        <CardSubtitle>Temperature</CardSubtitle>
+        {isImperial ? <CardText>{temp} F</CardText> : <CardText>{Math.round((temp - 32 ) / 1.8) } C</CardText>}
+        <Button color="success" onClick={toggleTempScale}>Switch C/F</Button>
+      </CardBody>
+    </Card>
   );
 };
 
