9ebf7e97cc7574ffa82227016606de8269ed8306
JVinson813 <JVinson813@gmail.com>
2020-10-27 09:15:33 -0400

    10/27

--- blue24app/src/App.jsx
+++ blue24app/src/App.jsx
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import ZomatoApi from './zomato/Zomato';
 
 function App() {
   return (
     <div className="App">
-    
+      <h2>HI</h2>
+      <ZomatoApi/>
     </div>
   );
 }
