b69f08db5595909124a0dd35612b9f5d1cd2e546
adamtaylorsmith <adamtaylor@icloud.com>
2020-10-27 09:17:51 -0400

    10-27

--- blue24app/src/App.js
+++ blue24app/src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+//import 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
+import NasaComponent from './components/NasaComponent'
 
 function App() {
   return (
     <div className="App">
-    
+      <div></div>
+    <NasaComponent />
     </div>
   );
 }
 
 export default App;
+
+//latitude={latitude} longitude={longitude}
---
+++ blue24app/src/components/NasaComponent.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+const baseURL = 'https://api.nasa.gov/planetary/earth/assets';
+const APIkey = '&api_key=8XcfrD7gyh4pLMLMQ3DR4BEOK4PeVIRpXyjCkzkI'; 
+const date = '&date=2020-10-26';
+const lat = '&lat=39.791000';
+const lon = '?lon=-86.148003';
+
+console.log('hello');
+
+const NasaComponent = (props) => {
+  const [results, setResults] = useState('');
+
+  fetch(baseURL+lon+lat+date+APIkey)
+  .then(response => response.json())
+  .then(json => setResults(json.url));
+
+  return (
+    <div>
+      <img className="satImg" src={results} />
+      {/* <p>The Longitude is: {props.latitude}</p>
+      <p>The Longitude is: {props.longitude}</p> */}
+    </div>
+  )
+}
+
+export default NasaComponent
+
