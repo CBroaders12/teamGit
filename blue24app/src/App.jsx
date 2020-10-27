e68fae660e2033dbd2f7a7fe53a134596dac5be9
adamtaylorsmith <adamtaylor@icloud.com>
2020-10-27 13:35:18 -0400

    update

--- blue24app/src/App.jsx
+++ blue24app/src/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 //import 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
+<<<<<<< HEAD:blue24app/src/App.js
 import NasaComponent from './components/NasaComponent'
+=======
+import ZomatoApi from './zomato/Zomato';
+>>>>>>> 9ebf7e9... 10/27:blue24app/src/App.jsx
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD:blue24app/src/App.js
       <div></div>
     <NasaComponent />
+=======
+      <h2>HI</h2>
+      <ZomatoApi/>
+>>>>>>> 9ebf7e9... 10/27:blue24app/src/App.jsx
     </div>
   );
 }
